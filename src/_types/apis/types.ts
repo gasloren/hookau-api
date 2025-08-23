d4165b89eab9814d5d7200f2bfd545f29141a444
Gaston Alberto Lorenzo Uhrich <gaston.lorenzo@gmail.com>
2025-08-23 15:39:18 -0300

    removed admin methods

--- src/_types/apis/types.ts
+++ src/_types/apis/types.ts
@@ -1,9 +1,7 @@
-import type { Controller as AdminController } from './admin.js';
 import type { Controller as BuyerController } from './buyer.js';
 import type { Controller as SessionController } from './session.js';
 
 export interface Controller {
-  admin: AdminController;
   buyer: BuyerController;
   session: SessionController;
 }
--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -2,7 +2,6 @@ import type { T } from '../_types/index.js';
 import type { IDatabase } from '../mongo/types.js';
 import type { IRedisDB } from '../redis/types.js';
 
-import { adminApiController } from './admin/admin.api.controller.js';
 import { buyerApiController } from './buyer/buyer.api.controller.js';
 import { sessionApiController } from './session/session.api.controller.js';
 
@@ -14,7 +13,6 @@ export function ApiController(
 ): T.Api.Controller {
 
   return {
-    admin: adminApiController(mdb),
     buyer: buyerApiController(mdb),
     session: sessionApiController(rdb)
   };
